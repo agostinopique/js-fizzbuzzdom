dde2894360cb7d40667e17021e976660ac13b7de
agostinopique <ago.pique@yahoo.it>
2022-04-08 17:43:31 +0200

    Commit Milestone 1; Aggiunto ciclo for con condizioni if;

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+
+
+// console.log('hi, i\'m steive');
+
+// Definisco il numero di cicli
+let rounds = 100;
+
+// Inizio il ciclo
+for(let i = 1; i <=30; i++){
+
+    // Definisco i casi di esecuzione
+    
+    if((!(i % 3)) && (!(i % 5))) {
+
+        console.log('FizzBuzz');
+
+    } else if(!(i % 3)) {
+        
+        console.log('Fizz');        
+
+
+    } else if(!(i % 5)) {
+
+        console.log('Buzz');
+
+    } else {
+
+        console.log(i);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
